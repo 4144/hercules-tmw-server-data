56b0770d8ad2ca051973031a659b42758a32282d
Andrei Karas <akaras@inbox.ru>
2015-01-01 16:35:04 +0300

    Remove useless files and move other files to correct place.

--- world/map/resources2.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-type: text/plain");
-header("Cache-Control: no-store, no-cache, must-revalidate"); 
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-
-print file_get_contents ("resources2.txt");
-?>
